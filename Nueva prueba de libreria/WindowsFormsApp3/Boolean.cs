abc8d61f88f15c2974fcf9b41539aba71edce50b
Octavio Salasar Ramírez <3AMSRO@GMAIL,COM>
2020-04-10 18:10:00 -0700

    Eliminación de más de una variable
    Ahora el método puede eliminar más de una variable

--- Nueva prueba de libreria/WindowsFormsApp3/Boolean.cs
+++ Nueva prueba de libreria/WindowsFormsApp3/Boolean.cs
@@ -63,7 +63,10 @@ namespace WindowsFormsApp3
             List<int> mov1 = new List<int>();
             List<int> mov2 = new List<int>();
             int c = 0;
-            int apuntador = 0;
+            //int apuntador = 0;
+            //Esta lista sirve para indicar que variables se van a eliminar
+            //a la hora de la simplificación
+            List<int> variablesEliminadas = new List<int>();
 
             for (int i = 0; i < expresionBooleana1.Count; i++)
             {
@@ -80,15 +83,17 @@ namespace WindowsFormsApp3
                 {
                     mov1.Add(expresionBooleana1[i]);
                     mov2.Add(expresionBooleana2[i]);
-                    apuntador = i;
+                    variablesEliminadas.Add(i);
                 }
             }
 
             //Si hay una variable diferente se puede cancelar
             if (c == 3)
-                return UnaVariable(mov1, mov2, expresionBooleana1, expresionBooleana2, apuntador);
-            else
+                return UnaVariable(mov1, mov2, expresionBooleana1, expresionBooleana2, variablesEliminadas[0]);
+            else if (c == 0)
                 return Iguales(expresionBooleana1, expresionBooleana2);
+            else
+                return VariasVariables(mov1, mov2, expresionBooleana1, expresionBooleana2, variablesEliminadas);
         }
 
         public static void Modificar(List<int> mov1, List<int> mov2, List<int> pol1, List<int> pol2)
@@ -102,6 +107,54 @@ namespace WindowsFormsApp3
             }
         }
 
+        //Método por si se eliminan varias variables
+        public static string VariasVariables(List<int> mov1, List<int> mov2, List<int> pol1, 
+            List<int> pol2, List<int> variablesAEliminar)
+        {
+            string resultado = "";
+            /*Si todas las variables quedan con signos diferentes eso quiere decir
+             * que cumple la regla A + !A = 1
+            */
+            if (variablesAEliminar.Count == 4)
+            {
+                Modificar(mov1, mov2, pol1, pol2);
+                return "1";
+            }
+            List<int> resnum = mov1;
+            foreach(int apuntador in variablesAEliminar)
+            {
+                resnum[apuntador] = 1;
+            }            
+            for(int i =0; i< pol1.Count; i++)
+            {
+                if (resnum[i] != 1 && pol1[i] == 1)
+                {
+                    if (i == 0)
+                        resultado = "A";
+                    else if (i == 1)
+                        resultado += "B";
+                    else if (i == 2)
+                        resultado += "C";
+                    else if (i == 3)
+                        resultado += "D";
+                }
+                else if (resnum[i] != 1 && pol1[i] == 2)
+                {
+                    if (i == 0)
+                        resultado = "!A";
+                    else if (i == 1)
+                        resultado += "!B";
+                    else if (i == 2)
+                        resultado += "!C";
+                    else if (i == 3)
+                        resultado += "!D";
+                }
+            }
+            Modificar(mov1, mov2, pol1, pol2);
+            return resultado;
+        }
+
+        //Método por si se elimina una variables
         public static string UnaVariable(List<int> mov1, List<int> mov2, List<int> pol1, List<int> pol2, int apuntador)
         {
             string res = "";
